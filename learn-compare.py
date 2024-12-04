236c8f543579e8a23c57080b90ba0f5973907af4
Stefan Grosser <13567009+Bierchermuesli@users.noreply.github.com>
2024-12-04 19:51:02 +0100

    add inline documentation links

--- learn-compare.py
+++ learn-compare.py
@@ -11,13 +11,13 @@ import xgboost as xgb
 import pandas as pd
 
 
-def calculate_metrics(y_test, y_pred, model_name, balance_method, results):
+def calculate_metrics(y_test, y_pred, model_name, method, results):
     accuracy = accuracy_score(y_test, y_pred)
     precision = precision_score(y_test, y_pred, average="weighted")
     f1 = f1_score(y_test, y_pred, average="weighted")
 
-    results.append((model_name, balance_method, accuracy, precision, f1))
-    print(f"{model_name} ({balance_method} Balanced): " f"Accuracy = {accuracy:.8f}, Precision = {precision:.8f}, F1-Score = {f1:.8f}")  # print with 8 digit precision
+    results.append((model_name, method, accuracy, precision, f1))
+    print(f"{model_name} (Method {method}): " f"Accuracy = {accuracy:.8f}, Precision = {precision:.8f}, F1-Score = {f1:.8f}")  # print with 8 digit precision
 
 
 data_cleaned = pd.read_csv("features-clean.csv")
@@ -35,20 +35,30 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, r
 
 results = []
 models = [  # models to be used
-#     ("Random Forest", RandomForestClassifier()),
-#     ("Decision Tree", DecisionTreeClassifier()),
-#     ("KNN", KNeighborsClassifier()),
-#     ("MLP Classifier", MLPClassifier(hidden_layer_sizes=(100, 50), max_iter=MLP_MAX_ITER)),
-#     ("Perceptron", Perceptron(max_iter=PR_MAX_ITER))
-    ("XGBoost", xgb.XGBClassifier()),
+    ("Random Forest", RandomForestClassifier()), #https://scikit-learn.org/1.5/modules/generated/sklearn.ensemble.RandomForestClassifier.html
+    ("Decision Tree", DecisionTreeClassifier()), #https://scikit-learn.org/1.5/modules/generated/sklearn.tree.DecisionTreeClassifier.html
+    ("KNN", KNeighborsClassifier()), #https://scikit-learn.org/1.5/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
+    ("MLP Classifier", MLPClassifier(hidden_layer_sizes=(100, 50), max_iter=MLP_MAX_ITER)), #https://scikit-learn.org/1.5/modules/generated/sklearn.neural_network.MLPClassifier.html
+    ("Perceptron", Perceptron(max_iter=PR_MAX_ITER)) #https://scikit-learn.org/1.5/modules/generated/sklearn.linear_model.Perceptron.html
+    ("XGBoost", xgb.XGBClassifier()), # https://xgboost.readthedocs.io/en/latest/python/python_api.html
 ]
 
 for i in ["None", "SMOTE"]:  # train models without and then with SMOTE applied
     if i == "SMOTE":
+        # Documentation: https://imbalanced-learn.org/stable/references/generated/imblearn.over_sampling.SMOTE.html
+
+        # print("Before SMOTE: ", X_train.shape, y_train.shape)
+        # print(y_train.value_counts().reset_index())
+        # dfboth=pd.concat([X_train,y_train],axis=1)
+        # dfboth.to_csv("before-smoth.csv", index=False)
+
         smote = SMOTE(random_state=RANDOM_STATE)
-        #print("Before SMOTE: ", X_train.shape, y_train.shape)
         X_train, y_train = smote.fit_resample(X_train, y_train)
-        #print("After SMOTE: ", X_train.shape, y_train.shape)
+        # print("After SMOTE: ", X_train.shape, y_train.shape)
+        # dfboth=pd.concat([X_train,y_train],axis=1)
+        # dfboth.to_csv("after-smoth.csv", index=False)
+        # print(y_train.value_counts().reset_index())
+
 
     for name, model in models:
         model.fit(X_train, y_train)  # train the model
@@ -60,7 +70,9 @@ for i in ["None", "SMOTE"]:  # train models without and then with SMOTE applied
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
-models_scaling_needed = [("Logistic Regression", LogisticRegression(max_iter=LR_MAX_ITER))]  # models that need scaling, StandardScaler in this case
+models_scaling_needed = [
+    ("Logistic Regression", LogisticRegression(max_iter=LR_MAX_ITER)) #https://scikit-learn.org/1.5/modules/generated/sklearn.linear_model.LogisticRegression.html
+    ]  # models that need scaling, StandardScaler in this case
 
 
 for i in ["None", "SMOTE"]:
@@ -76,6 +88,6 @@ for i in ["None", "SMOTE"]:
 
 
 # Convert results to DataFrame for easier plotting
-results_df = pd.DataFrame(results, columns=["Model", "Balance", "Accuracy", "Precision", "F1-Score"])
+results_df = pd.DataFrame(results, columns=["Model", "Method", "Accuracy", "Precision", "F1-Score"])
 
 results_df.to_csv("learn-compare.csv", index=False)
--- visualize.py
+++ visualize.py
@@ -1,9 +1,14 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import json
 
 results_df = pd.read_csv('learn-compare.csv')
+json.dump(results_df.to_dict(), open('learn-compare.json', 'w'))
 results_df = pd.DataFrame(results_df)
+
+
+
 
 # Bar plot for all metrics?
 fig, axes = plt.subplots(1, 3, figsize=(18, 6))
@@ -19,7 +24,7 @@ y_max = results_df[metrics].max().max()+0.1  # Maximum of all metrics
 
 
 # Accuracy plot
-acc_plot = results_df.pivot(index="Model", columns="Balance", values="Accuracy").plot(kind='bar', ax=axes[0])
+acc_plot = results_df.pivot(index="Model", columns="Method", values="Accuracy").plot(kind='bar', ax=axes[0])
 axes[0].set_title('Accuracy Comparison')
 axes[0].set_ylabel('Accuracy')
 axes[0].set_xlabel('Model')
@@ -31,7 +36,7 @@ for container in acc_plot.containers:
 
 
 # Precision plot
-prec_plot = results_df.pivot(index="Model", columns="Balance", values="Precision").plot(kind='bar', ax=axes[1])
+prec_plot = results_df.pivot(index="Model", columns="Method", values="Precision").plot(kind='bar', ax=axes[1])
 axes[1].set_title('Precision Comparison')
 axes[1].set_ylabel('Precision')
 axes[1].set_xlabel('Model')
@@ -42,7 +47,7 @@ for container in prec_plot.containers:
     )
 
 # F1-Score plot
-f1_plot = results_df.pivot(index="Model", columns="Balance", values="F1-Score").plot(kind='bar', ax=axes[2])
+f1_plot = results_df.pivot(index="Model", columns="Method", values="F1-Score").plot(kind='bar', ax=axes[2])
 axes[2].set_title('F1-Score Comparison')
 axes[2].set_ylabel('F1-Score')
 axes[2].set_xlabel('Model')
@@ -61,7 +66,7 @@ plt.show()
 
 # or as as heatmap?
 import seaborn as sns
-pivot_data = results_df.pivot(index="Model", columns="Balance")
+pivot_data = results_df.pivot(index="Model", columns="Method")
 fig, axes = plt.subplots(1, 3, figsize=(18, 6))
 
 
