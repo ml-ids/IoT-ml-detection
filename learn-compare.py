6736566c9dac8b84aa0cc6a5a0ab9904728f9d7d
KBak <56936481+K-Bach@users.noreply.github.com>
2024-12-07 16:37:11 +0100

    Karim feature selection (#1)
    * add .gitignore and feature type definitions; refactor feature preprocessing
    
    * huh
    
    * add variance analysis
    
    * sort variances in descending order and remove plotting code
    
    * refactor
    
    * fix: correct syntax by adding missing comma in model list
    
    * add KBest selection and refactor
    
    * refactor: update feature definitions and enhance KBest selection process
    
    * enhance KBest processing logic
    
    * fix
    
    * fix
    
    * refactor
    
    * huh

--- learn-compare.py
+++ learn-compare.py
@@ -9,85 +9,67 @@ from imblearn.over_sampling import SMOTE
 from sklearn.preprocessing import StandardScaler
 import xgboost as xgb
 import pandas as pd
+from typing import List, Tuple
 
-
-def calculate_metrics(y_test, y_pred, model_name, method, results):
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred, average="weighted")
-    f1 = f1_score(y_test, y_pred, average="weighted")
-
-    results.append((model_name, method, accuracy, precision, f1))
-    print(f"{model_name} (Method {method}): " f"Accuracy = {accuracy:.8f}, Precision = {precision:.8f}, F1-Score = {f1:.8f}")  # print with 8 digit precision
-
-
-data_cleaned = pd.read_csv("csvs/features-clean.csv")
-data_cleaned = pd.DataFrame(data_cleaned)
-
+# Constants
+DATASET_PATH = 'csvs/datasets/'
+FSELECTION_PATH = 'csvs/fSelection/'
+MODELS_COMPARISON_PATH = 'csvs/modelsComparison/'
+DATASET_NAME = 'binary_10best_features.csv'
 RANDOM_STATE = 42
 LR_MAX_ITER = 300
 MLP_MAX_ITER = 500
 PR_MAX_ITER = 300
 TEST_SIZE = 0.3
 
-X = data_cleaned.drop("label", axis=1)  # features
-y = data_cleaned["label"]  # target
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, random_state=RANDOM_STATE)  # split into 0.7 data, 0.3 test data ratio
+def apply_smote(X_train: pd.DataFrame, y_train: pd.Series, random_state: int):
+    smote = SMOTE(random_state=random_state)
+    return smote.fit_resample(X_train, y_train)
+
+def calculate_metrics(y_test: pd.Series, y_pred: pd.Series, model_name: str, method: str, results: List[Tuple[str, str, float, float, float]]):
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred, average="weighted")
+    f1 = f1_score(y_test, y_pred, average="weighted")
+    results.append((model_name, method, accuracy, precision, f1))
+    print(f"{model_name} (Method {method}): Accuracy = {accuracy:.8f}, Precision = {precision:.8f}, F1-Score = {f1:.8f}")
+
+def train_and_evaluate(models: List[Tuple[str, object]], X_train: pd.DataFrame, y_train: pd.Series, X_test: pd.DataFrame, y_test: pd.Series, method: str, results: List[Tuple[str, str, float, float, float]]):
+    for name, model in models:
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        calculate_metrics(y_test, y_pred, name, method, results)
+
+
+data_cleaned = pd.read_csv(f"{FSELECTION_PATH}{DATASET_NAME}")
+X = data_cleaned.drop("label", axis=1)
+y = data_cleaned["label"]
+X_train, X_test, y_train, y_test =  train_test_split(X, y, test_size=TEST_SIZE, random_state=RANDOM_STATE)
 
 results = []
-models = [  # models to be used
-    ("Random Forest", RandomForestClassifier()), #https://scikit-learn.org/1.5/modules/generated/sklearn.ensemble.RandomForestClassifier.html
-    ("Decision Tree", DecisionTreeClassifier()), #https://scikit-learn.org/1.5/modules/generated/sklearn.tree.DecisionTreeClassifier.html
-    ("KNN", KNeighborsClassifier()), #https://scikit-learn.org/1.5/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
-    ("MLP Classifier", MLPClassifier(hidden_layer_sizes=(100, 50), max_iter=MLP_MAX_ITER)), #https://scikit-learn.org/1.5/modules/generated/sklearn.neural_network.MLPClassifier.html
-    ("Perceptron", Perceptron(max_iter=PR_MAX_ITER)) #https://scikit-learn.org/1.5/modules/generated/sklearn.linear_model.Perceptron.html
-    ("XGBoost", xgb.XGBClassifier()), # https://xgboost.readthedocs.io/en/latest/python/python_api.html
+models = [
+    ("Random Forest", RandomForestClassifier()),
+    ("Decision Tree", DecisionTreeClassifier()),
+    ("KNN", KNeighborsClassifier()),
+    ("MLP Classifier", MLPClassifier(hidden_layer_sizes=(100, 50), max_iter=MLP_MAX_ITER)),
+    ("Perceptron", Perceptron(max_iter=PR_MAX_ITER)),
+    ("XGBoost", xgb.XGBClassifier())
 ]
 
-for i in ["None", "SMOTE"]:  # train models without and then with SMOTE applied
-    if i == "SMOTE":
-        # Documentation: https://imbalanced-learn.org/stable/references/generated/imblearn.over_sampling.SMOTE.html
-
-        # print("Before SMOTE: ", X_train.shape, y_train.shape)
-        # print(y_train.value_counts().reset_index())
-        # dfboth=pd.concat([X_train,y_train],axis=1)
-        # dfboth.to_csv("before-smoth.csv", index=False)
-
-        smote = SMOTE(random_state=RANDOM_STATE)
-        X_train, y_train = smote.fit_resample(X_train, y_train)
-        # print("After SMOTE: ", X_train.shape, y_train.shape)
-        # dfboth=pd.concat([X_train,y_train],axis=1)
-        # dfboth.to_csv("after-smoth.csv", index=False)
-        # print(y_train.value_counts().reset_index())
-
-
-    for name, model in models:
-        model.fit(X_train, y_train)  # train the model
-        y_pred = model.predict(X_test)  # predictions
-
-        calculate_metrics(y_test, y_pred, name, i, results)
-
+for method in ["None", "SMOTE"]:
+    if method == "SMOTE":
+        X_train, y_train = apply_smote(X_train, y_train, RANDOM_STATE)
+    train_and_evaluate(models, X_train, y_train, X_test, y_test, method, results)
 
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
-models_scaling_needed = [
-    ("Logistic Regression", LogisticRegression(max_iter=LR_MAX_ITER)) #https://scikit-learn.org/1.5/modules/generated/sklearn.linear_model.LogisticRegression.html
-    ]  # models that need scaling, StandardScaler in this case
+models_scaling_needed = [("Logistic Regression", LogisticRegression(max_iter=LR_MAX_ITER))]
 
+for method in ["None", "SMOTE"]:
+    if method == "SMOTE":
+        X_train_scaled, y_train = apply_smote(X_train_scaled, y_train, RANDOM_STATE)
+    train_and_evaluate(models_scaling_needed, X_train_scaled, y_train, X_test_scaled, y_test, method, results)
 
-for i in ["None", "SMOTE"]:
-    if i == "SMOTE":
-        smote = SMOTE(random_state=RANDOM_STATE)
-        X_train_scaled, y_train_scaled = smote.fit_resample(X_train_scaled, y_train)
-
-    for name, model in models_scaling_needed:
-        model.fit(X_train_scaled, y_train)  # train the model
-        y_pred = model.predict(X_test_scaled)  # predictions
-
-        calculate_metrics(y_test, y_pred, name, i, results)
-
-
-# Convert results to DataFrame for easier plotting
 results_df = pd.DataFrame(results, columns=["Model", "Method", "Accuracy", "Precision", "F1-Score"])
+results_df.to_csv(f"{MODELS_COMPARISON_PATH}{DATASET_NAME.replace('.csv', '')}_compare.csv", index=False)
 
-results_df.to_csv("csvs/learn-compare.csv", index=False)
