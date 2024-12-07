125ffed5ee694d4107a7cb7d14e5a8a0c4ae72cd
KBak <karimbachir2001@gmail.com>
2024-12-07 13:37:02 +0100

    refactor: update feature definitions and enhance KBest selection process

--- features-clean.py
+++ features-clean.py
@@ -1,14 +1,33 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.calibration import LabelEncoder
-from sklearn.compose import ColumnTransformer
 from sklearn.discriminant_analysis import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectKBest, VarianceThreshold, mutual_info_classif
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import f1_score, precision_score, accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.tree import DecisionTreeClassifier
+import xgboost as xgb
 # Import the feature types definition
 from featureTypesDef import *
+import os
+
+RANDOM_STATE = 42
+LR_MAX_ITER = 300
+MLP_MAX_ITER = 500
+PR_MAX_ITER = 300
+TEST_SIZE = 0.3
+
+models = [  # models to be used
+    ("Random Forest", RandomForestClassifier()), #https://scikit-learn.org/1.5/modules/generated/sklearn.ensemble.RandomForestClassifier.html
+    ("Decision Tree", DecisionTreeClassifier()), #https://scikit-learn.org/1.5/modules/generated/sklearn.tree.DecisionTreeClassifier.html
+    ("KNN", KNeighborsClassifier()), #https://scikit-learn.org/1.5/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
+    ("MLP Classifier", MLPClassifier(hidden_layer_sizes=(100, 50), max_iter=MLP_MAX_ITER)), #https://scikit-learn.org/1.5/modules/generated/sklearn.neural_network.MLPClassifier.html
+    ("Perceptron", Perceptron(max_iter=PR_MAX_ITER)), #https://scikit-learn.org/1.5/modules/generated/sklearn.linear_model.Perceptron.html
+    ("XGBoost", xgb.XGBClassifier()) # https://xgboost.readthedocs.io/en/latest/python/python_api.html
+]
 
 def scaleEncode(x):
     print("# Scaling numerical features...")
@@ -56,7 +75,6 @@ def getVariances(threshold, data):
     return variances_df, features_high_variance, features_low_variance
 
 def getKBest(k, data, target, targetName):
-    k = 1
     # selection
     selector = SelectKBest(score_func=mutual_info_classif, k=k)
     x_kbest = selector.fit_transform(data.drop(columns=targetName), target)
@@ -66,7 +84,12 @@ def getKBest(k, data, target, targetName):
     x_kbest_df = pd.DataFrame(x_kbest, columns=kbest_features)
 
     return x_kbest_df
-    
+
+def calculate_metrics(y_test, y_pred, model_name):
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred, average="weighted")
+    f1 = f1_score(y_test, y_pred, average="weighted")
+    return (model_name, accuracy, precision, f1)    
 
 # Load the data
 dataset_path = 'csvs/datasets/'
@@ -77,6 +100,7 @@ data = pd.read_csv(f'{dataset_path}train_test_network.csv',
                    true_values=['T', 't', '1'], false_values=['F', 'f', '0'],
                    na_values=['-'])
 print('# Data loaded successfully')
+print(f"# Features [{data.columns.size}]: ", sorted(data.columns))
 
 #Value counts for classes
 print("# Value counts for binary classes:")
@@ -122,18 +146,46 @@ features_high_variance.to_csv(f'{fSelection_path}features_high_variance.csv')
 features_low_variance.to_csv(f'{fSelection_path}features_low_variance.csv')
 
 # Apply KBest
-# print("# Applying KBest...")
-k = 1
-print("# Applying KBest for binary target...")
-x_binary_kbest_df = getKBest(k, data_binary, y_binary, 'label')
-print("# Applying KBest for multi-class target...")
-x_multi_kbest_df = getKBest(k, data_multi, y_multi, 'type')
-x_binary_kbest_df['label'] = y_binary.values
-x_binary_kbest_df.to_csv(f'{fSelection_path}binary_{k}best_features.csv', index=False)
-x_multi_kbest_df['type'] = y_multi.values
-x_multi_kbest_df.to_csv(f'{fSelection_path}multi_{k}best_features.csv', index=False)
+results = []
+# for k in range(1, 3 + 1):
+for k in range(1, data_noLabel.shape[1] + 1):
+    file_path = f'{fSelection_path}binary_{k}best_features.csv'
+    try:
+        x_binary_kbest_df = pd.read_csv(file_path).drop(columns=['label'])
+        print(f"# {file_path} already exists. Skipping KBest computation for k={k}.")
+        x_train, x_test, y_train, y_test = train_test_split(x_binary_kbest_df, y_binary, test_size=TEST_SIZE, random_state=RANDOM_STATE)
+    except FileNotFoundError:
+        print(f"# Applying KBest for binary target with k={k}...")
+        x_binary_kbest_df = getKBest(k, data_binary, y_binary, 'label')
+        x_train, x_test, y_train, y_test = train_test_split(x_binary_kbest_df, y_binary, test_size=TEST_SIZE, random_state=RANDOM_STATE)
+        # Save the k-best features to a CSV file
+        x_binary_kbest_df['label'] = y_binary.values
+        x_binary_kbest_df.to_csv(file_path, index=False)
+    # train and predict the model
+    name, model = models[0]
+    print(f"# Training...")
+    model.fit(x_train, y_train)  
+    print(f"# Predicting...")
+    y_pred = model.predict(x_test)
+    # metrics
+    modelName, accuracy, precision, f1 = calculate_metrics(y_test, y_pred, name)
+    kbest_features_names = x_binary_kbest_df.columns.tolist()
+    results.append((k, modelName, accuracy, precision, f1, kbest_features_names))
+    print(f"{modelName} (k={k}): " f"Accuracy = {accuracy:.8f}, Precision = {precision:.8f}, F1-Score = {f1:.8f}")  # print with 8 digit precision
+    
+    # print(f"# Applying KBest for multi-class target with k={k}...")
+    # x_multi_kbest_df = getKBest(k, data_multi, y_multi, 'type')
+    
+results_df = pd.DataFrame(results, columns=["K", "Model", "Accuracy", "Precision", "F1-Score", "Features"])
+results_df.to_csv(f'{fSelection_path}binary_kbest_{modelName}_results.csv', index=False)
+# results_df = pd.DataFrame(results, columns=["K", "Model", "Accuracy", "Precision", "F1-Score", "Features"])
+# results_df.to_csv(f'{fSelection_path}multi_kbest_{modelName}_results.csv', index=False)
 
 print(f"# Results in {fSelection_path}")
+
+
+
+
 
 # correlation_matrix = data_cleaned.corr()
 # plt.figure(figsize=(20, 20))
